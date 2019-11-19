8a61acc0a3624f47f2388d4ae3aac76238c8aeb3
pscxtc <009000*a>
2019-11-19 11:38:25 +0800

    取消 @EnableCircuitBreaker 增加 zuul.ribbon.eager-load.enabled = true

--- movie/src/main/java/com/example/demo/MovieApplication.java
+++ movie/src/main/java/com/example/demo/MovieApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableCircuitBreaker
 public class MovieApplication {
 
     @Bean
--- user/src/main/java/com/example/demo/UserApplication.java
+++ user/src/main/java/com/example/demo/UserApplication.java
@@ -2,12 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
 public class UserApplication {
 
 	public static void main(String[] args) {
