bc1c1585d9240704238e0d470038b9bd08e61bd6
pscxtc <009000*a>
2019-11-21 11:51:23 +0800

    自动部署等脚本

---
+++ stackCode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+codePath="/root/gitSpace/h2movie/";#自己的项目路径
+action=$1;#通过调用传入  [./updateCode.sh  test1]  test1 就是 $1
+project=$2;
+if [ ! -n "$1" ] ;then
+  action="deploy";
+fi
+
+if [ ! -n "$2" ] ;then
+  project="h2";
+fi
+case $action in
+    deploy) echo -e "部署";docker stack deploy -c docker-compose.yml $project;;
+    rm) echo -e "删除";docker stack rm $project;;
+    logs) echo -e "日志";docker service logs -f $project;;
+    update) echo -e "强制更新";docker service update --force --detach=false $project;;
+    *) echo -e "参数异常!可选[deploy/rm/logs/update]";;
+esac
