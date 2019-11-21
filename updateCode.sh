d8dd012a88a588cd3e7d75fe7f5cd45bc687f342
pscxtc <009000*a>
2019-11-21 08:00:29 +0800

    自动 git pull 和 mvn clean package 脚本

---
+++ updateCode.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+codePath="/root/gitSpace/h2movie/";#自己的项目路径
+project=$1;#通过调用传入  [./updateCode.sh  test1]  test1 就是 $1
+if [ ! -n "$1" ] ;then
+  projectName="test";# 默认项目名  也可以搞个输入框自己输入
+fi
+cd $codePath$projectName
+echo "git 更新代码开始。。。。。" ;
+git pull origin master #正常更新
+#git fetch --all ;
+#git reset --hard origin/master ; #覆盖更新
+echo "git 更新代码完成。。。。。";
+mvn clean package;
+docker image prune;
+echo y;
+docker images;
