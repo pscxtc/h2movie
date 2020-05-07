615a324df4ec4c6c904ccf2d2c4a51c7a67d1462
pscxtc <009000*>
2020-05-07 13:47:16 +0800

    1.新增 路径打开jar包脚本

---
+++ restartByPath.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#根据当前路径,循环启动其下所有 jar文件,并且在打开日志,在日志打印至 Started 下一行时,关闭日志
+cur_Dir=$(pwd)
+target=$1;
+jars=`find ${cur_Dir}/${target} -name '*.jar'`;
+for state in ${jars}
+do
+	echo "部署 ${state##*/}"
+	pid=`ps -ef | grep ${state##*/}  | grep -v grep  | awk '{print $2}'`
+	echo “旧应用进程id：$pid”
+	if [ -n "$pid" ]
+	then
+		for kid in ${pid}
+		do
+        		kill -9 $kid
+        		echo "kill $kid 完毕"
+		done
+	fi
+	nohup java -Xms256m -Xmx512m -jar ${state} --spring.cloud.nacos.config.server-addr=192.168.17.63:8848  --spring.cloud.nacos.config.namespace=57f1e614-0d90-47a7-9181-726d1a51cdc5 --spring.profiles.active=dev  > ${state%/*}.log 2>&1 &
+	echo  "打开日志 ${state%/*}.log"
+        tail -200f ${state%/*}.log|sed -e '/Started*/{n;Q}'
+done
